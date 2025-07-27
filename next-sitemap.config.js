dc586434691599dd31f735cbd88fcfef4cfe6f40
Roshan Sharma <rsharma@Roshans-MacBook-Pro.local>
2025-07-27 12:33:02 -0700

    feat: Add next-sitemap configuration

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://roshansharma1.github.io',
+  generateRobotsTxt: true, 
+}
--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  siteUrl: 'https://roshansharma1.github.io',
+  generateRobotsTxt: true,
 };
 
 export default nextConfig;
